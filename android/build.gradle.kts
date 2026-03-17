3501c4839e0b21345c160b64c457505bf7fc73a9
Elvin Bernabe <bernabeelvin@email.com>
2026-03-17 12:39:38 +0800

    Remove afterEvaluate fix

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -22,16 +22,3 @@ subprojects {
 tasks.register<Delete>("clean") {
     delete(rootProject.layout.buildDirectory)
 }
-
-subprojects {
-    afterEvaluate {
-        if (this is com.android.build.gradle.AppExtension) {
-            buildTypes {
-                getByName("release") {
-                    isShrinkResources = false
-                    isMinifyEnabled = false
-                }
-            }
-        }
-    }
-}
